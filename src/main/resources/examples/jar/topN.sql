1aba4205d23726b6d0e6a755b3063d3df52c47d8
gx <gxlevi123@163.com>
2022-01-27 11:21:26 +0800

    first commit

--- src/main/resources/examples/jar/topN.sql
+++ src/main/resources/examples/jar/topN.sql
@@ -1,4 +1,11 @@
-SELECT supplier, sum(price) AS total
+CREATE TABLE IF NOT EXISTS top_price(
+    price Float64,
+    supplier String,
+    start DateTime,
+    end   DateTime
+) engine TinyLog;
+
+SELECT supplier, SUM(price) AS total
 FROM top_price
 WHERE start > '2022-01-26 10:53:00'
 GROUP BY supplier
